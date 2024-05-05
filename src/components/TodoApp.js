876529e6ae248cb9e9eb95fcf2ead6f5df307863
sooraj vijayan <soorajtk3@gmail.com>
2024-05-05 23:10:04 +0530

    Adds todo delete functionality

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,65 +1,84 @@
 import React, { useState } from "react";
 import { StyledTodo } from "./styles";
-
+import trash from "../assets/trash.svg";
 const TodoApp = () => {
   const [text, setText] = useState("");
   const [todos, setTodos] = useState([]);
   const handleButtonClick = () => {
-    setTodos([...todos,{'todo':text,status:''}]);
+    setTodos([...todos, { todo: text, status: "" }]);
     setText("");
   };
   console.log(todos);
 
   const handleChange = (index) => {
-    // setChecked(!checked)
     const updatedTodos = todos.map((todo, i) =>
-      i === index ? { ...todo, status: todo.status === "done" ? "" : "done" } : todo
+      i === index
+        ? { ...todo, status: todo.status === "done" ? "" : "done" }
+        : todo
     );
     setTodos(updatedTodos);
   };
+  const handleDeleteTodo = (index) => {
+    const updatedTodos = [...todos];
+    updatedTodos.splice(index, 1);
+    setTodos(updatedTodos);
+  };
+
   return (
     <StyledTodo>
-    <div className="mainContainer">
-      <div className="">
-        <h3>Todo Details</h3>
-      </div>
-      <input
-        type="text"
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-      />
+      <div className="mainContainer">
+        <div className="">
+          <h3 style={{textAlign:'center',color:'red'}}>Enter Todo Details</h3>
+        </div>
+        <div className="inputTodo">
+          <input
+          placeholder="Add your Todo Tasks..."
+            className="inputField"
+            type="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
 
-      <button onClick={handleButtonClick}>Add Todo</button>
-      <div style={{ display: "flex",flexDirection:'column' }}>
+          <button className="addTodoBtn" onClick={handleButtonClick}>
+            Add Todo
+          </button>
+        </div>
         <div>
-          <h4>Todo List</h4>
+         {todos.length >0 && <h4 style={{color:'#B22222'}}>Todo List</h4>} 
           {todos?.map((todo, index) => (
             <>
-            <div key={index}>
-              <label key={index}>{todo?.todo}</label>
-              <input type="checkbox" checked={todo?.status==='done'} onChange={()=>handleChange(index)}/>
-              <br/>
-            </div>
+              <div className="todoDetails" key={index}>
+                <label className="todoLabel" key={index}>{todo?.todo}</label>
+                <div>
+                <input
+                  style={{ cursor: "pointer" }}
+                  type="checkbox"
+                  checked={todo?.status === "done"}
+                  onChange={() => handleChange(index)}
+                />
+                <img
+                  style={{ cursor: "pointer",marginLeft:'8px' }}
+                  onClick={() => handleDeleteTodo(index)}
+                  src={trash}
+                />
+                </div>
+              </div>
+              <br />
             </>
           ))}
         </div>
-        <br/>
+        <br />
         <div>
-          <h4>Completed</h4>
+         {todos.length >0 && <h4 style={{color:'#B22222'}}>Completed</h4>} 
           {todos?.map((todo, index) => (
             <>
-            <div key={index}>
-            {todo.status==='done' &&
-              <h3>{todo?.todo}</h3>
-            }
-     
-             
-            </div>
+              <div key={index}>
+                {todo.status === "done" && <p className="completedText">{todo?.todo}</p>}
+              </div>
             </>
           ))}
         </div>
       </div>
-    </div>
     </StyledTodo>
   );
 };
--- src/components/styles.js
+++ src/components/styles.js
@@ -1,16 +1,46 @@
-
-import styled from "@emotion/styled"
+import styled from "@emotion/styled";
 
 export const StyledTodo = styled.div`
-
-.mainContainer{
-margin:20px auto;
-max-width:560px;
-border-radius:8px;
-background: #f1f1f1;
-padding:15px 18px
-}
-
-
-
-`
+  .mainContainer {
+    margin: 20px auto;
+    max-width: 500px;
+    border-radius: 8px;
+    background: #f1f1f1;
+    padding: 15px 24px;
+  }
+  .inputField {
+    padding: 8px 16px;
+    border-radius: 8px;
+    border: 1px solid lightgray;
+  }.inputTodo{
+    display:flex;
+    gap:8px;
+    justify-content: center;
+    width: 100%;
+  }.addTodoBtn{
+    padding:5px 10px;
+    border-radius:10px;
+    background: rgb(252, 168, 168);
+    color: rgb(252, 37, 37);
+    border: none;
+    cursor: pointer;
+    font-size: 14px;
+    font-weight: 600
+  }.todoDetails{
+    display: flex;
+    justify-content: space-between;
+    gap: 12px;
+    width: 100%;
+    padding: 12px;
+    background: rgb(252, 225, 105);
+    margin-bottom: 8px;
+    box-sizing: border-box
+  }.todoLabel{
+    color: rgb(92, 75, 1);
+    font-weight: 600;
+  }.completedText{
+    color: rgb(245, 94, 94);
+    font-weight: 600;
+    font-size: 16px;
+  }
+`;
